b9a12b971abb43db2ec0a6c09da7492b656c4f46
solebox <5013box@gmail.com>
2019-08-03 22:49:26 +0300

    added docker-compose for db and dump for users database init

---
+++ dump.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE USERS;
+USE USERS;
+CREATE TABLE USERS(username varchar(1000), password varchar(1000), email varchar(1000), score int);
+INSERT INTO USERS values('abc', '32424', 'abd@gmail.com', 0);
+
