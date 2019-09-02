3acdb6152d43414777023213c8734b27ab112166
orenklein455 <51347312+orenklein455@users.noreply.github.com>
2019-09-02 22:26:14 +0300

    as you can see this is the change we made, lets commit and push

--- src/main/java/dto/Player.java
+++ src/main/java/dto/Player.java
@@ -1,52 +1,61 @@
 package dto;
 
+//import com.google.gson.annotations.SerializedName;
+
 public class Player {
+    //    @SerializedName("userName")
     private String userName;
-    private String password;
-    private String email;
-    private int score;
-    private GameRoom room;
-    private boolean connected;
+//    @SerializedName("password")
+//    private String password;
+//    @SerializedName("email")
+//    private String email;
+//    @SerializedName("score")
+//    private int score;
+//    @SerializedName("room")
+//    private GameRoom room;
+//    @SerializedName("connected")
+//    private boolean connected;
 
 
     public String getUserName() {
         return userName;
     }
 
-    public String getPassword() {
-        return password;
-    }
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public int getScore() {
+//        return score;
+//    }
+//
+//    public GameRoom getRoom() {
+//        return room;
+//    }
+//
+//    public boolean isConnected() {
+//        return connected;
+//    }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public GameRoom getRoom() {
-        return room;
-    }
-
-    public boolean isConnected() {
-        return connected;
-    }
-
-    public Player(String userName, String password, String email) {
+    //    public Player(String userName, String password, String email) {
+    public Player(String userName) {
         this.userName = userName;
-        this.password = password;
-        this.email = email;
-        this.score = 0;
-        connected = false;
+//        this.password = password;
+//        this.email = email;
+//        this.score = 0;
+//        connected = false;
     }
 
-    @Override
-    public boolean equals(Object obj) { //add this method to other objects as well
-        Player p = (Player)obj;
-        if ((this.userName.equals(p.userName)) || (this.email.equals(p.email)))
-                return true;
-        return false;
-    }
+//    @Override
+//    public boolean equals(Object obj) { //add this method to other objects as well
+//        Player p = (Player)obj;
+//        if ((this.userName.equals(p.userName)) || (this.email.equals(p.email)))
+//                return true;
+//        return false;
+//    }
 
 }
