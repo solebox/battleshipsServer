3e5b57e451c2b77ea9024cb2391c767ae56c2396
Koby Kilimnik <koby.kilimnik@imperva.com>
2019-09-15 18:33:14 +0300

    added dump

--- dump.sql
+++ dump.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE USERS;
 USE USERS;
-CREATE TABLE USERS(username varchar(1000), password varchar(1000), email varchar(1000), score int);
-INSERT INTO USERS values('abc', '32424', 'abd@gmail.com', 0);
+CREATE TABLE Users(username varchar(1000), password varchar(1000), email varchar(1000), score int);
+INSERT INTO Users values('abc', '32424', 'abd@gmail.com', 0);
+INSERT INTO Users values('shimi', '123456', 'shimi@gmail.com', 0);
+INSERT INTO Users values('kk', '123', 'kk@gmail.com', 0);
+
+
+GRANT ALL PRIVILEGES ON *.* TO 'oren'@'%' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON *.* TO 'oren'@'localhost' IDENTIFIED BY '1234';
 
